336e30d523c424ee78b025b95791e6d46782e342
Dalaktronixs <hakeemabdullah87@gmail.com>
2024-10-07 17:51:35 +0100

    FEAT: Implement the autosuspend features using the background.js file

--- background/background.js
+++ background/background.js
@@ -2,3 +2,31 @@ chrome.runtime.onInstalled.addListener(function() {
     console.log("Tab Manager Extension installed!");
   });
   
+  const inactiveTimeLimit = 600000; // 10 minutes (in milliseconds)
+let lastActiveTabs = {};
+
+chrome.tabs.onActivated.addListener(activeInfo => {
+    const tabId = activeInfo.tabId;
+    const windowId = activeInfo.windowId;
+    lastActiveTabs[windowId] = tabId;
+});
+
+// Function to suspend inactive tabs
+function autoSuspendTabs() {
+    chrome.windows.getAll({populate: true}, function(windows) {
+        windows.forEach(window => {
+            window.tabs.forEach(tab => {
+                const now = new Date().getTime();
+                const lastActiveTime = lastActiveTabs[window.id] || now;
+
+                // If tab has been inactive for more than 10 minutes
+                if (now - lastActiveTime >= inactiveTimeLimit && !tab.audible) {
+                    chrome.tabs.discard(tab.id);
+                }
+            });
+        });
+    });
+}
+
+// Check every minute to suspend inactive tabs
+setInterval(autoSuspendTabs, 60000);
