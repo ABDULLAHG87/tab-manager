8d4152374da34bc553d3ad57179f59e107930ac5
Dalaktronixs <hakeemabdullah87@gmail.com>
2024-10-07 18:08:10 +0100

    REFACTOR: modify code for autosuspend of inactive tabs using the background.js file

--- background/background.js
+++ background/background.js
@@ -1,32 +1,32 @@
-chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(() => {
     console.log("Tab Manager Extension installed!");
   });
   
-  const inactiveTimeLimit = 600000; // 10 minutes (in milliseconds)
-let lastActiveTabs = {};
-
-chrome.tabs.onActivated.addListener(activeInfo => {
-    const tabId = activeInfo.tabId;
-    const windowId = activeInfo.windowId;
+  const inactiveTimeLimit = 600000; // 10 minutes in milliseconds
+  let lastActiveTabs = {};
+  
+  chrome.tabs.onActivated.addListener(activeInfo => {
+    const { tabId, windowId } = activeInfo;
     lastActiveTabs[windowId] = tabId;
-});
-
-// Function to suspend inactive tabs
-function autoSuspendTabs() {
-    chrome.windows.getAll({populate: true}, function(windows) {
-        windows.forEach(window => {
-            window.tabs.forEach(tab => {
-                const now = new Date().getTime();
-                const lastActiveTime = lastActiveTabs[window.id] || now;
-
-                // If tab has been inactive for more than 10 minutes
-                if (now - lastActiveTime >= inactiveTimeLimit && !tab.audible) {
-                    chrome.tabs.discard(tab.id);
-                }
-            });
+  });
+  
+  // Auto-suspend inactive tabs
+  function autoSuspendTabs() {
+    chrome.windows.getAll({ populate: true }, function(windows) {
+      windows.forEach(window => {
+        const now = Date.now();
+        const lastActiveTime = lastActiveTabs[window.id] || now;
+  
+        window.tabs.forEach(tab => {
+          // Suspend if inactive for more than 10 minutes
+          if (now - lastActiveTime >= inactiveTimeLimit && !tab.audible) {
+            chrome.tabs.discard(tab.id);
+          }
         });
+      });
     });
-}
-
-// Check every minute to suspend inactive tabs
-setInterval(autoSuspendTabs, 60000);
+  }
+  
+  // Check every minute to suspend inactive tabs
+  setInterval(autoSuspendTabs, 60000);
+  
